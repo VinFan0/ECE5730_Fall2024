dd60550ab45846249f6a74921214d79ebf09220f
Ryan Beck <ryan.beck1820@gmail.com>
2024-09-21 03:14:50 +0100

    Commented stopwatch.vhd and created stopwatch_TB.vhd

--- Lab2/stopwatch.vhd
+++ Lab2/stopwatch.vhd
@@ -5,14 +5,15 @@ use ieee.numeric_std.all;
 entity stopwatch is
 	
 	generic(
+		-- T is number of clock cycles for each tick of the counter
 		T : integer := 100_000
 	);
 	
 	port (
 		-- CLK input
 		ADC_CLK_10 : in std_logic;		-- 10 MHz
-		MAX10_CLK1_50 : in std_logic;	-- 50 MHz 1
-		MAX10_CLK2_50 : in std_logic;	-- 50 MHz 2
+		-- MAX10_CLK1_50 : in std_logic;	-- 50 MHz 1
+		-- MAX10_CLK2_50 : in std_logic;	-- 50 MHz 2
 		
 		-- Button input
 		KEY : in std_logic_vector(1 downto 0);
@@ -23,9 +24,10 @@ entity stopwatch is
 		HEX2 : out std_logic_vector(7 downto 0);
 		HEX3 : out std_logic_vector(7 downto 0);
 		HEX4 : out std_logic_vector(7 downto 0);
-		HEX5 : out std_logic_vector(7 downto 0)
+		HEX5 : out std_logic_vector(7 downto 0);
+		HEX6 : out std_logic_vector(7 downto 0)
 		);
-end entity stopwatch5
+end entity stopwatch;
 
 architecture behavioral of stopwatch is
 
@@ -36,19 +38,17 @@ begin
 
 	process (ADC_CLK_10, KEY)
 	begin
-		if KEY(0) = '0' then
+		if KEY(0) = '0' then -- Reset behavior
 			count <= 0;
 			start <= 0;
-		elsif KEY(1) = '0' then
-			if KEY(1) = '1' then
-				start <= 1;
-			end if;
-		elsif start = 1 && rising_edge(ADC_CLK_10) then
+		elsif KEY(1) = '0' then -- Start pressed
+			start <= 1; -- RB you had this nested in another if KEY(1) = '1', which wouldn't do anything
+		elsif start = 1 and rising_edge(ADC_CLK_10) then -- If timing
 			if count < T then
-				count <= count + 1;
+				count <= count + 1; -- Increment count
 			else 
-				count <= 0;
+				count <= 0; -- Reset after reaching T value
 			end if;
 		end if;
-
+	end process;
 end architecture behavioral;
---
+++ Lab2/stopwatch_TB.vhd
@@ -0,0 +1,82 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity stopwatch_TB is
+end entity stopwatch_TB;
+
+architecture behavioral of stopwatch_TB is
+
+	component stopwatch is
+		generic (
+			-- T: how many clock cycles until count increments
+			T : integer := 5
+		);
+		port (
+			-- Inputs
+			ADC_CLK_10 : in std_logic;
+			KEY : in std_logic_vector(1 downto 0);
+
+			-- Outputs
+			HEX0: out std_logic_vector(7 downto 0);
+			HEX1: out std_logic_vector(7 downto 0);
+			HEX2: out std_logic_vector(7 downto 0);
+			HEX3: out std_logic_vector(7 downto 0);
+			HEX4: out std_logic_vector(7 downto 0);
+			HEX5: out std_logic_vector(7 downto 0);
+			HEX6: out std_logic_vector(7 downto 0)
+		);
+	end component;
+
+	signal ADC_CLK_10 : std_logic := '0';
+	signal KEY : std_logic_vector(1 downto 0);
+	signal HEX0 : std_logic_vector(7 downto 0);
+	signal HEX1 : std_logic_vector(7 downto 0);
+	signal HEX2 : std_logic_vector(7 downto 0);
+	signal HEX3 : std_logic_vector(7 downto 0);
+	signal HEX4 : std_logic_vector(7 downto 0);
+	signal HEX5 : std_logic_vector(7 downto 0);
+	signal HEX6 : std_logic_vector(7 downto 0);
+
+	constant CLK_PERIOD : time := 10 ns;
+
+begin
+	uut : stopwatch
+		generic map (
+			T => 5
+		)
+		port map (
+			ADC_CLK_10 => ADC_CLK_10,
+			KEY => KEY,
+			HEX0 => HEX0,
+			HEX1 => HEX0,
+			HEX2 => HEX0,
+			HEX3 => HEX0,
+			HEX4 => HEX0,
+			HEX5 => HEX0,
+			HEX6 => HEX0
+		);
+
+		clk_process : process
+		begin
+			ADC_CLK_10 <= '0';
+			wait for CLK_PERIOD / 2;
+			ADC_CLK_10 <= '1';
+			wait for CLK_PERIOD / 2;
+		end process;
+
+		stm_process : process
+		begin
+			KEY(0) <= '1'; 			-- Initial RST button
+			KEY(1) <= '1';			-- Initail START button
+			wait for CLK_PERIOD * 5;
+			KEY(0) <= '0';			-- Toggle RST
+			wait for CLK_PERIOD * 5;
+			KEY(0) <= '1';
+			wait for CLK_PERIOD * 5;
+			KEY(1) <= '0';			-- Toggle START
+			wait for CLK_PERIOD * 5;
+			KEY(1) <= '1';
+			wait;
+		end process;
+end architecture behavioral;		
