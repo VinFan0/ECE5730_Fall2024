5fcbb6c01cde6d1d7035b4a82c22f7010dfef6b6
Ryan Beck <ryan.beck1820@gmail.com>
2024-09-29 06:01:09 +0100

    Updated rng.vhd with comments, and functionality to constantly be generating numbers, but only display on button press.

--- Lab3/rng.vhd
+++ Lab3/rng.vhd
@@ -29,46 +29,60 @@ entity rng is
 		HEX2 : out std_logic_vector(7 downto 0);
 		HEX3 : out std_logic_vector(7 downto 0);
 		HEX4 : out std_logic_vector(7 downto 0);
-		HEX5 : out std_logic_vector(7 downto 0);
-		HEX6 : out std_logic_vector(7 downto 0)
+		HEX5 : out std_logic_vector(7 downto 0)
 	);
 
 end entity rng;
 
 architecture behavioral of rng is
 
-	signal lfsr : STD_LOGIC_VECTOR(15 downto 0);
-   signal bit1  : STD_LOGIC;
-	signal place1 : integer := 0;
-	signal place2 : integer := 0;
+	signal lfsr : STD_LOGIC_VECTOR(15 downto 0) := "1010010110001011";	-- LFSR defaults to A58B
+   	signal bit1  : STD_LOGIC;
+	signal place1 : integer;
+	signal place2 : integer;
 	signal start : integer := 0;
 	
 	type SEVEN_SEG is array (0 to 15) of std_logic_vector(7 downto 0); -- Define new type for lookup table
-	constant table : SEVEN_SEG := (X"C0", X"F9", X"A4", X"B0", X"99",  -- 0, 1, 2, 3, 4
-       				  X"92", X"82", X"F8", X"80", X"90",	-- 5, 6, 7, 8, 9
-						  X"88", X"83", X"C6", X"A1", X"86", X"8E"); --A, B, C, D, E, F
+	constant table : SEVEN_SEG := (	
+					X"C0", X"F9", X"A4", X"B0",  -- 0, 1, 2, 3
+       					X"99", X"92", X"82", X"F8",  -- 4, 5, 6, 7
+					X"80", X"90", X"88", X"83",  -- 8, 9, A, B
+					X"C6", X"A1", X"86", X"8E"); -- C, D, E, F
 
 begin
 	-- Define module behavior here --
 	process (ADK_CLK_10, KEY) -- Sensitivity list goes in ()
 	begin
-		if KEY(0) = '0' or start = 0 then
+		if KEY(0) = '0' then
 			-- Reset behavior --
+			ready <= '1';
+			place1 <= X"B";
+			place2 <= X"8";
+			HEX0 <= table(place1); 	--Display seed value
+			HEX1 <= table(place2); 	--Display seed value
 			lfsr <= seed; --lfsr equals seed value
-			
 		elsif KEY(1) = '0' then
-			start <= 1;
-			bit1 <= (lfsr(0) xor lfsr(2) xor lfsr(3) xor lfsr(5)) and '1';
-			lfsr <= lfsr(14 downto 0) & bit1;
+			-- Want to only show a new number when button pressed --
+			if ready = 1 then
+				HEX0 <= table(place1); -- update first digit display
+				HEX1 <= table(place2); -- upadate second digit display
+				ready <= '0';	
+			end if
+		else
+			ready <= '1';
 		end if;
-		place1 <= to_integer(unsigned(lfsr(3 downto 0))); --converting last 4 bits of lfsr to integer
+		-- Update LFSR constantly to assist in randomness --
+
+		bit1 <= (lfsr(0) xor lfsr(2) xor lfsr(3) xor lfsr(5)) and '1';  -- Tabs: 16, 14, 13, 11
+		lfsr <= lfsr(14 downto 0) & bit1; -- update lfsr with generated number
+		place1 <= to_integer(unsigned(lfsr(3 downto 0))); --converting bits 3-0 of lfsr to integer
 		place2 <= to_integer(unsigned(lfsr(7 downto 4))); --converting bits 4-7 of lfsr to integer
-		HEX0 <= table(place1);--Display value
-		HEX1 <= table(place2);--Display value
+
 	end process;
 	
 	process (ADK_CLK_10)
 	begin
+		-- Clear unused 7-segments
 		HEX2 <= X"FF"; --Display off
 		HEX3 <= X"FF"; --Display off
 		HEX4 <= X"FF"; --Display off
--- Lab3/rng_TB.vhd
+++ Lab3/rng_TB.vhd
@@ -2,50 +2,76 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity TB_NAME is
-end entity TB_NAME;
+entity rng_TB is
+end entity rng_TB; 
 
-architecture behavioral of TB_NAME is
+architecture behavioral of rng_TB is
 
 	-- Instantiate component(s) to test --
-	component MODULE is
+	component rng is
 		generic (
 			-- Provide generic values --
-
+			seed : std_logic_vector(15 downto 0) := X"A58B"
 		);
 		port (
 			-- Declare ports --
 
 			-- Inputs --
 			-- Clocks --
-			ADC_CLK_10 	: in std_logic;
-			MAX10_CLK1_50 	: in std_logic;
-			MAX10_CLK2_50 	: in std_logic;
+			ADC_CLK_10 : in std_logic;
+			-- MAX10_CLK1_50 	: in std_logic;
+			-- MAX10_CLK2_50 	: in std_logic;
 
 			-- Buttons --
 			KEY : in std_logic_vector(1 downto 0);
 
 			-- Outputs --
 			-- LEDs --
-			LEDR : out std_logic_vector(9 downto 0);
+			-- LEDR : out std_logic_vector(9 downto 0);
+			
+			-- 7 Segment --
+			HEX0 : out std_logic_vector(7 downto 0);
+			HEX1 : out std_logic_vector(7 downto 0);
+			HEX2 : out std_logic_vector(7 downto 0);
+			HEX3 : out std_logic_vector(7 downto 0);
+			HEX4 : out std_logic_vector(7 downto 0);
+			HEX5 : out std_logic_vector(7 downto 0)
 		);
 	end component;
 
 	-- Define internal signals/values
 
+	signal ADC_CLK_10 : std_logic := '0';
+	signal KEY : std_logic_vector(1 downto 0);
+	signal HEX0 : std_logic_vector(7 downto 0);
+	signal HEX1 : std_logic_vector(7 downto 0);
+	signal HEX2 : std_logic_vector(7 downto 0);
+	signal HEX3 : std_logic_vector(7 downto 0);
+	signal HEX4 : std_logic_vector(7 downto 0);
+	signal HEX5 : std_logic_vector(7 downto 0);
+
+
 	constant CLK_PERIOD : time := 10 ns;
 begin
 
 	-- Define unit under test --
-	uut : MODULE
+	uut : rng
 		generic map (
 			-- Map generic values (separated by , )--
 			-- NAME => value --
-
+			seed => X"A58B"
 		)
 		port map (
 			-- Map port connections --
 			-- NAME => NAME (separated by , ) --
+			ADC_CLK_10 => ADC_CLK_10,
+			KEY => KEY,
+			HEX0 => HEX0,
+			HEX1 => HEX1,
+			HEX2 => HEX2,
+			HEX3 => HEX3,
+			HEX4 => HEX4,
+			HEX5 => HEX5
 		);
 
 		-- Define processes --
@@ -64,6 +90,7 @@ begin
 			
 			-- Initial values --
 			KEY(0) <= '1';
+			
 
 			wait for CLK_PERIOD * 10; 
 			KEY(0) <= '0';            -- Initial RESET --
