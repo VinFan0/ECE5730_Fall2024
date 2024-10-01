ef27440c0a6598ed460131ec40258b20b9d40a37
Ryan Beck <ryan.beck1820@gmail.com>
2024-10-01 23:07:24 +0100

    Initial commit Lab 4

---
+++ Lab4/place.cpp
@@ -0,0 +1,14 @@
+/*
+ * ECE 5730
+ * Lab4 - Simulated Annealing
+ * 10/10/24
+ * Ryan Beck, Jared Bronson
+ *
+ */
+#include <iostream>
+
+int main() {
+	std::cout << "TEST" << std::endl;
+
+	return 0;
+}
