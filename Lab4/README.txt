fcfdb34083e7a35231d64c0394359bad93538cb6
Ryan Beck <ryan.beck1820@gmail.com>
2024-10-02 04:33:22 +0100

    Just playing with c++ classes and a README

---
+++ Lab4/README.txt
@@ -0,0 +1,17 @@
+Simulated Annealing
+ ECE 5730 / Lab 4 / 10/10/24
+ Ryan Beck, Jared Bronson
+
+ Compiling Code
+----------------------------
+The code complies via g++
+    g++ -o build place.cpp
+
+ Running Program
+----------------------------
+The program is run via command line
+    ./buile.exe
+
+The default grid created for placing nodes is 5x5 with 5 nodes. 
+The user may include a single integer commandline argument to change the number of nodes.i.e.
+    ./build.exe 4   would create a 5x5 with 4 nodes.
--- Lab4/place.cpp
+++ Lab4/place.cpp
@@ -6,9 +6,68 @@
  *
  */
 #include <iostream>
+#include <vector>
 
-int main() {
-	std::cout << "TEST" << std::endl;
+class Grid {
+	private:
+		int rows;
+		int cols;
+		std::vector<std::vector<char>> grid;
+	public:
+		Grid(int rows, int cols) : rows(rows), cols(cols), grid(rows,std::vector<char>(cols,'-')) {}
+
+
+		void setValue(int i, int j, char value) {
+			if (i >= 0 && i < rows && j >= 0 && j < cols) 
+				grid[i][j] = value;
+			else
+				std::cout << "Index out of bounds!" << std::endl;
+		}
+
+		int getValue(int i, int j) const {
+			if (i >= 0 && i < rows && j >= 0 && j < cols) {
+				return grid[i][j];
+			} else {
+				std::cout << "Index out fo bounds!" << std::endl;
+				return -1;
+			}
+		}
+
+		void printGrid() {
+			for(int i=0; i<rows; ++i) {
+				for(int j=0; j<cols; ++j) {
+					std::cout << grid[i][j] << " " ;
+				}
+				std::cout << std::endl;
+			}
+			std::cout << std::endl;
+		}
+};
+
+int main(int argc, char *argv[]) {
+	int rows = 5;
+	int cols  = 5;
+	int nodes = 5;
+	if(argc > 1) {
+		std::cout << "Updating node count to " << argv[1] << std::endl;
+		nodes = atoi(argv[1]);
+	}
+	else {
+		std::cout << "Creating default 5x5" << std::endl;
+	}
+	Grid myGrid(rows,cols);
+	myGrid.printGrid();
+	
+	int row,col,val;
+	
+	for(int count=0;count<nodes;++count) {
+		std::cout << "What row? ";
+		std::cin >> row;
+		std::cout << "What col? ";
+		std::cin >> col;
+		myGrid.setValue(row-1,col-1, char(count+48));
+		myGrid.printGrid();
+	}
 
 	return 0;
 }
