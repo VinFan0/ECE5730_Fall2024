8271d888b251c5cf44a6b1003448a7c060073858
jarebear98 <jared.bronson98@gmail.com>
2024-10-07 22:07:48 -0600

    change

--- Lab4/place.h
+++ Lab4/place.h
@@ -6,6 +6,6 @@ void copy(int *current, int *next);
 void alter(int *next);
 int evaluate(int *next);
 void accept(int *current_val, int next_val, int *current, int *next, float temperature);
-float adjust_temperature();
+float cooling();
 
 #endif
