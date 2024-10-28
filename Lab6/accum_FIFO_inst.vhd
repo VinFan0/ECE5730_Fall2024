d6a027091819b7cbca131789934a310994db13e3
Ryan Beck <ryan.beck1820@gmail.com>
2024-10-28 15:23:56 +0100

    Added FIFO, need to update signal names

---
+++ Lab6/accum_FIFO_inst.vhd
@@ -0,0 +1,11 @@
+accum_FIFO_inst : accum_FIFO PORT MAP (
+		clock	 => clock_sig,
+		data	 => data_sig,
+		rdreq	 => rdreq_sig,
+		sclr	 => sclr_sig,
+		wrreq	 => wrreq_sig,
+		empty	 => empty_sig,
+		full	 => full_sig,
+		q	 => q_sig,
+		usedw	 => usedw_sig
+	);
