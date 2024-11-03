f7b6867be4d561d5863f4192c39155894f17a60b
jarebear98 <jared.bronson98@gmail.com>
2024-11-02 22:18:56 -0600

    Flag changes

--- Lab7/Vhdl1.vhd
+++ Lab7/Vhdl1.vhd
@@ -1,194 +1,194 @@
 --Flag 0 - France
 if (pix_count > 427) and (pix_count <= 640) then
 	--BLUE = #002395
-	VGA_R <= '0000';
-	VGA_G <= '0010';
-	VGA_B <= '1001';
+	next_next_VGA_R <= "0000";
+	next_next_VGA_G <= "0010";
+	next_next_VGA_B <= "1001";
 elsif (pix_count > 213) and (pix_count <= 427) then
 	--White = #FFFFFF
-	VGA_R <= '1111';
-	VGA_G <= '1111';
-	VGA_B <= '1111';
+	next_next_VGA_R <= "1111";
+	next_next_VGA_G <= "1111";
+	next_next_VGA_B <= "1111";
 elsif pix_count <= 213 then
 	--RED = #ed2939
-	VGA_R <= '1110';
-	VGA_G <= '0010';
-	VGA_B <= '0011';
+	next_next_VGA_R <= "1110";
+	next_next_VGA_G <= "0010";
+	next_next_VGA_B <= "0011";
 end if;
 	
 --Flag 1-Italy
 if (pix_count > 427) and (pix_count <= 640) then
 	--GREEN = #009246
-	VGA_R <= '0000';
-	VGA_G <= '1001';
-	VGA_B <= '0100';
+	next_next_VGA_R <= "0000";
+	next_next_VGA_G <= "1001";
+	next_next_VGA_B <= "0100";
 elsif (pix_count > 213) and (pix_count <= 427) then
 	--White = #FFFFFF
-	VGA_R <= '1111';
-	VGA_G <= '1111';
-	VGA_B <= '1111';
+	next_next_VGA_R <= "1111";
+	next_next_VGA_G <= "1111";
+	next_next_VGA_B <= "1111";
 elsif pix_count <= 213 then
 	--RED = #ce2b37
-	VGA_R <= '1100';
-	VGA_G <= '0010';
-	VGA_B <= '0011';
+	next_next_VGA_R <= "1100";
+	next_next_VGA_G <= "0010";
+	next_next_VGA_B <= "0011";
 end if;
 	
 --Flag 2-Ireland
 if (pix_count > 427) and (pix_count <= 640) then
 	--GREEN = #169b62
-	VGA_R <= '0001';
-	VGA_G <= '1001';
-	VGA_B <= '0110';
+	next_next_VGA_R <= "0001";
+	next_next_VGA_G <= "1001";
+	next_next_VGA_B <= "0110";
 elsif (pix_count > 213) and (pix_count <= 427) then
 	--White = #FFFFFF
-	VGA_R <= '1111';
-	VGA_G <= '1111';
-	VGA_B <= '1111';
+	next_next_VGA_R <= "1111";
+	next_next_VGA_G <= "1111";
+	next_next_VGA_B <= "1111";
 elsif pix_count <= 213 then
 	--ORANGE = #ff883e
-	VGA_R <= '1111';
-	VGA_G <= '1000';
-	VGA_B <= '0011';
+	next_next_VGA_R <= "1111";
+	next_next_VGA_G <= "1000";
+	next_next_VGA_B <= "0011";
 end if;	
 	
 --Flag 3-Belgium
 if (pix_count > 427) and (pix_count <= 640) then
 	--BLACK = #000000
-	VGA_R <= '0000';
-	VGA_G <= '0000';
-	VGA_B <= '0000';
+	next_next_VGA_R <= "0000";
+	next_next_VGA_G <= "0000";
+	next_next_VGA_B <= "0000";
 elsif (pix_count > 213) and (pix_count <= 427) then
 	--Yellow = #fae042
-	VGA_R <= '1111';
-	VGA_G <= '1110';
-	VGA_B <= '0100';
+	next_next_VGA_R <= "1111";
+	next_next_VGA_G <= "1110";
+	next_next_VGA_B <= "0100";
 elsif pix_count <= 213 then
 	--RED = #ed2939
-	VGA_R <= '1110';
-	VGA_G <= '0010';
-	VGA_B <= '0011';
+	next_next_VGA_R <= "1110";
+	next_next_VGA_G <= "0010";
+	next_next_VGA_B <= "0011";
 end if;
 
 --Flag 4-Mali	
 if (pix_count > 427) and (pix_count <= 640) then
 	--GREEN = #14b53a
-	VGA_R <= '0001';
-	VGA_G <= '1011';
-	VGA_B <= '0011';
+	next_next_VGA_R <= "0001";
+	next_next_VGA_G <= "1011";
+	next_next_VGA_B <= "0011";
 elsif (pix_count > 213) and (pix_count <= 427) then
 	--Yellow = #fcd116
-	VGA_R <= '1111';
-	VGA_G <= '1101';
-	VGA_B <= '0001';
+	next_next_VGA_R <= "1111";
+	next_next_VGA_G <= "1101";
+	next_next_VGA_B <= "0001";
 elsif pix_count <= 213 then
 	--RED = #ce1126
-	VGA_R <= '1100';
-	VGA_G <= '0001';
-	VGA_B <= '0010';
+	next_next_VGA_R <= "1100";
+	next_next_VGA_G <= "0001";
+	next_next_VGA_B <= "0010";
 end if;
 
 --Flag 5-Chad	
 if (pix_count > 427) and (pix_count <= 640) then
 	--BLUE = #002664
-	VGA_R <= '0000';
-	VGA_G <= '0010';
-	VGA_B <= '0110';
+	next_VGA_R <= "0000";
+	next_VGA_G <= "0010";
+	next_VGA_B <= "0110";
 elsif (pix_count > 213) and (pix_count <= 427) then
 	--Yellow = #fecb00
-	VGA_R <= '1111';
-	VGA_G <= '1100';
-	VGA_B <= '0000';
+	next_VGA_R <= "1111";
+	next_VGA_G <= "1100";
+	next_VGA_B <= "0000";
 elsif pix_count <= 213 then
 	--RED = #c60c30
-	VGA_R <= '1100';
-	VGA_G <= '0000';
-	VGA_B <= '0011';
+	next_VGA_R <= "1100";
+	next_VGA_G <= "0000";
+	next_VGA_B <= "0011";
 end if;
 
 --Flag 6-Nigeria
 if (pix_count > 427) and (pix_count <= 640) then
 	--GREEN = #008751
-	VGA_R <= '0000';
-	VGA_G <= '1000';
-	VGA_B <= '0101';
+	next_VGA_R <= "0000";
+	next_VGA_G <= "1000";
+	next_VGA_B <= "0101";
 elsif (pix_count > 213) and (pix_count <= 427) then
 	--WHITE = #FFFFFF
-	VGA_R <= '1111';
-	VGA_G <= '1111';
-	VGA_B <= '1111';
+	next_VGA_R <= "1111";
+	next_VGA_G <= "1111";
+	next_VGA_B <= "1111";
 elsif pix_count <= 213 then
 	--GREEN = #008751
-	VGA_R <= '0000';
-	VGA_G <= '1000';
-	VGA_B <= '0101';
+	next_VGA_R <= "0000";
+	next_VGA_G <= "1000";
+	next_VGA_B <= "0101";
 end if;
 
 --Flag 7-Ivory Coast
 if (pix_count > 427) and (pix_count <= 640) then
 	--Orange = #f77f00
-	VGA_R <= '1111';
-	VGA_G <= '1001';
-	VGA_B <= '0000';
+	next_VGA_R <= "1111";
+	next_VGA_G <= "1001";
+	next_VGA_B <= "0000";
 elsif (pix_count > 213) and (pix_count <= 427) then
 	--WHITE = #FFFFFF
-	VGA_R <= '1111';
-	VGA_G <= '1111';
-	VGA_B <= '1111';
+	next_VGA_R <= "1111";
+	next_VGA_G <= "1111";
+	next_VGA_B <= "1111";
 elsif pix_count <= 213 then
 	--GREEN = #009e60
-	VGA_R <= '0000';
-	VGA_G <= '1001';
-	VGA_B <= '0110';
+	next_VGA_R <= "0000";
+	next_VGA_G <= "1001";
+	next_VGA_B <= "0110";
 end if;
 
 --Flag 8-Poland
 if (lin_count > 45) and (lin_count <= 285) then
 	--WHITE = #FFFFFF
-	VGA_R <= '1111';
-	VGA_G <= '1111';
-	VGA_B <= '1111';
+	next_VGA_R <= "1111";
+	next_VGA_G <= "1111";
+	next_VGA_B <= "1111";
 elsif (lin_count > 285) and (lin_count <= 525) then
 	--RED = #dc143c
-	VGA_R <= '1101';
-	VGA_G <= '0001';
-	VGA_B <= '0011';
+	next_VGA_R <= "1101";
+	next_VGA_G <= "0001";
+	next_VGA_B <= "0011";
 end if;
 
 --Flag 9-Germany
 if (lin_count > 45) and (lin_count <= 205) then
 	--BLACK = #000000
-	VGA_R <= '0000';
-	VGA_G <= '0000';
-	VGA_B <= '0000';
+	next_VGA_R <= "0000";
+	next_VGA_G <= "0000";
+	next_VGA_B <= "0000";
 elsif (lin_count > 205) and (lin_count <= 365) then
 	--RED = #dd0000
-	VGA_R <= '1101';
-	VGA_G <= '0000';
-	VGA_B <= '0000';
+	next_VGA_R <= "1101";
+	next_VGA_G <= "0000";
+	next_VGA_B <= "0000";
 elsif (lin_count > 365) and (lin_count <= 525) then
 	--YELLOW = #ffce00
-	VGA_R <= '1111';
-	VGA_G <= '1100';
-	VGA_B <= '0000';
+	next_VGA_R <= "1111";
+	next_VGA_G <= "1100";
+	next_VGA_B <= "0000";
 end if;
 
 --Flag 10-Austria
 if (lin_count > 45) and (lin_count <= 205) then
 	--RED = #ed2939
-	VGA_R <= '1110';
-	VGA_G <= '0010';
-	VGA_B <= '0011';
+	next_VGA_R <= "1110";
+	next_VGA_G <= "0010";
+	next_VGA_B <= "0011";
 elsif (lin_count > 205) and (lin_count <= 365) then
 	--WHITE = #FFFFFF
-	VGA_R <= '1111';
-	VGA_G <= '1111';
-	VGA_B <= '1111';
+	next_VGA_R <= "1111";
+	next_VGA_G <= "1111";
+	next_VGA_B <= "1111";
 elsif (lin_count > 365) and (lin_count <= 525) then
 	--RED = #ed2939
-	VGA_R <= '1110';
-	VGA_G <= '0010';
-	VGA_B <= '0011';
+	next_VGA_R <= "1110";
+	next_VGA_G <= "0010";
+	next_VGA_B <= "0011";
 end if;
 	
 --Flag 11-Republic of Congo
@@ -196,19 +196,19 @@ i = 160
 j = 0
 if (pix_count > i) and (pix_count <= 640) then
 	--GREEN = #009543
-	VGA_R <= '0000';
-	VGA_G <= '1001';
-	VGA_B <= '0100';
+	next_VGA_R <= "0000";
+	next_VGA_G <= "1001";
+	next_VGA_B <= "0100";
 elsif(pix_count <= i) and (pix_count > j) then
 	--YELLOW = #fbde4a
-	VGA_R <= '1111';
-	VGA_G <= '1101';
-	VGA_B <= '0100';
+	next_VGA_R <= "1111";
+	next_VGA_G <= "1101";
+	next_VGA_B <= "0100";
 elsif(pix_count <= j) and (pix_count > 0) then
 	--RED = #dc241f
-	VGA_R <= '1101';
-	VGA_G <= '0010';
-	VGA_B <= '0001';
+	next_VGA_R <= "1101";
+	next_VGA_G <= "0010";
+	next_VGA_B <= "0001";
 end if;
 i++
 j++
